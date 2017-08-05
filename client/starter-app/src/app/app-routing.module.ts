76d61c004e696afe4285a03f08c6242974a512f4
ashwin-sekhar <ashwin.sekhar@people10.com>
2017-08-05 13:57:23 +0530

    resolve conflect

--- client/starter-app/src/app/app-routing.module.ts
+++ client/starter-app/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { AppComponent } from './app.component';
-//import { TimeSheetComponent } from './time-sheet/time-sheet.component';
-import { LoginComponent } from './login/login.component';
-
-const routes: Routes = [
-  { path: '', redirectTo:'login', pathMatch:'full'},
-  {path: 'login', component:LoginComponent},
-=======
 import { TimeSheetComponent } from './time-sheet/time-sheet.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -20,8 +12,9 @@ const routes: Routes = [
     path: '', 	
     component:TimeSheetComponent,
     children: []
-  }
->>>>>>> 8b0a1869290c7a120c6e4b972c95fbad09b7412b
+  }, {path: 'login',
+   component:LoginComponent}
+
 ];
 
 @NgModule({
--- client/starter-app/src/app/app.module.ts
+++ client/starter-app/src/app/app.module.ts
@@ -4,20 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-//import { TimeSheetComponent } from './time-sheet/time-sheet.component';
 import { LoginComponent } from './login/login.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-=======
+
 import { TimeSheetComponent } from './time-sheet/time-sheet.component';
 import { DatePickerModule } from 'ng2-datepicker';
->>>>>>> 8b0a1869290c7a120c6e4b972c95fbad09b7412b
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    //TimeSheetComponent,
     LoginComponent
   ],
   imports: [
@@ -25,11 +22,8 @@ import { DatePickerModule } from 'ng2-datepicker';
     FormsModule,
     HttpModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    BsDropdownModule.forRoot()
-=======
+    BsDropdownModule.forRoot(),
     DatePickerModule
->>>>>>> 8b0a1869290c7a120c6e4b972c95fbad09b7412b
   ],
   providers: [],
   bootstrap: [AppComponent]
